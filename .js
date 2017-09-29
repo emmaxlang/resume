31babad3b742d779de421079db1f745c7e275df4
emmaxlang <31938633+emmaxlang@users.noreply.github.com>
2017-09-28 19:09:24 -0700

    Update .js
    incomplete skeleton for a link to email

--- .js
+++ .js
@@ -1,1 +1,5 @@
 
+var link = document.createElement('a');
+link.textContent = 'emma.b.lang@gmail.com';
+link.href = 'INSERTLINKTOEMAILHERE.COM';
+document.getElementById('where_to_insert').appendChild(link); //code retrieved from https://stackoverflow.com/questions/8005694/make-hyperlink-from-javascript
